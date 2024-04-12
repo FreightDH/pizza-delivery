0d49e2629a96944247b02a2010c8e5d4bbecc80f
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-04-12 17:02:40 +0300

    feat: add Header component

---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,46 @@
+import type { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+import { CustomButton } from '@/shared/UI/CustomButton';
+
+import { cartIcon, logo, profileIcon } from './assets';
+import cl from './Header.module.scss';
+
+export const Header: FC = (): ReactElement => {
+  return (
+    <header className={cl.header}>
+      <div className="header__container">
+        <div className={cl.header__body}>
+          <Link className={cl.logo} to="/">
+            <img alt="logo" src={logo} />
+            <div>
+              <h1 className={cl.logo__title}>Pizza Delivery</h1>
+              <div className={cl.logo__subtitle}>самая вкусная пицца во вселенной</div>
+            </div>
+          </Link>
+          <div className={cl.contacts}>
+            <div className={cl.contacts__phone}>8-800-123-45-67</div>
+            <p className={cl.contacts__text}>Звонок по России бесплатный</p>
+          </div>
+          <div className={cl.controls}>
+            <Link className={cl.controls__profile} to="/profile">
+              <img alt="profile-icon" src={profileIcon} />
+            </Link>
+            <Link to="/cart">
+              <CustomButton primary>
+                <div className={cl.button__body}>
+                  <div>0 ₽</div>
+                  <div className={cl.button__divider}></div>
+                  <div className={cl.button__count}>
+                    <img alt="cart-icon" src={cartIcon} />
+                    <span>0</span>
+                  </div>
+                </div>
+              </CustomButton>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/widgets/Header/assets/index.ts
@@ -0,0 +1,5 @@
+import logo from './logo.svg';
+import profileIcon from './profile.svg';
+import cartIcon from './cart.svg';
+
+export { logo, profileIcon, cartIcon };
