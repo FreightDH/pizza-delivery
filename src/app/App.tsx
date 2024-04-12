8127d03ee30c1c2ecf51e37c2168193544208e2c
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-04-12 17:04:36 +0300

    refactor: some files refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,3 @@ import type { ReactElement } from 'react';
 import { ReactRouterProvider } from './provider';
 
 export const App = (): ReactElement => <ReactRouterProvider />;
-
-// const App = (): ReactElement => {
-//   return (
-//     <>
-//       <main className={cl.page}>
-//         <div className="page__container">
-//           <div className={cl.page__body}></div>
-//         </div>
-//       </main>
-//     </>
-//   );
-// };
-
-// export default App;
