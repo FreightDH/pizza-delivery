8ddcb5cc17a285d451cd6a8b487efb3099706daa
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-04-25 19:09:53 +0300

    feat: add Footer component

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -2,12 +2,14 @@ import type { FC, ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@/widgets/Header';
+import { Footer } from '@/widgets/Footer';
 
 export const Layout: FC = (): ReactElement => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
