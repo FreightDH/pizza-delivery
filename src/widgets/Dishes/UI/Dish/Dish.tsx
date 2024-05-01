b49eb83f4213bfec71e50bf9777a6f8717fb2b7d
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-05-01 11:25:43 +0300

    feat: add Dish component

---
+++ src/widgets/Dishes/UI/Dish/Dish.tsx
@@ -0,0 +1,71 @@
+import type { MouseEvent } from 'react';
+import { useState, type FC, type ReactElement } from 'react';
+
+import { cn } from '@/shared/lib';
+import { CustomButton } from '@/shared/UI/CustomButton';
+
+import cl from './Dish.module.scss';
+
+interface DishProps {
+  img: string;
+  name: string;
+  price: number[];
+}
+
+export const Dish: FC<DishProps> = ({ img, name, price }): ReactElement => {
+  const [size, setSize] = useState('30 см');
+  const [dough, setDough] = useState('тонкое');
+
+  const handleClick = (e: MouseEvent<HTMLDivElement>, type: 'dought' | 'size') => {
+    const { textContent } = e.target as HTMLElement;
+
+    if (type === 'dought') {
+      setDough(textContent!);
+    } else {
+      setSize(textContent!);
+    }
+  };
+
+  return (
+    <div className={cl.dish}>
+      <div className={cl.dish__image}>
+        <img alt={name} src={img} />
+      </div>
+      <div className={cl.dish__name}>{name}</div>
+      <div className={cl.dish__controls}>
+        <div className={cl.controls__row}>
+          <div
+            className={cn(cl.controls__item, { [cl.active]: dough === 'тонкое' })}
+            onClick={(e) => handleClick(e, 'dought')}
+          >
+            тонкое
+          </div>
+          <div
+            className={cn(cl.controls__item, { [cl.active]: dough === 'традиционное' })}
+            onClick={(e) => handleClick(e, 'dought')}
+          >
+            традиционное
+          </div>
+        </div>
+        <div className={cl.controls__row}>
+          <div
+            className={cn(cl.controls__item, { [cl.active]: size === '30 см' })}
+            onClick={(e) => handleClick(e, 'size')}
+          >
+            30 см
+          </div>
+          <div
+            className={cn(cl.controls__item, { [cl.active]: size === '40 см' })}
+            onClick={(e) => handleClick(e, 'size')}
+          >
+            40 см
+          </div>
+        </div>
+      </div>
+      <div className={cl.dish__footer}>
+        <div className={cl.dish__price}>{size === '30 см' ? price[0] : price[1]} ₽</div>
+        <CustomButton className={cl.dish__btn}>+ Добавить</CustomButton>
+      </div>
+    </div>
+  );
+};
