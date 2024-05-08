5f32d162d7ff19b62b58a36404c94000b686c277
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-05-08 19:20:22 +0300

    feat: add MenuBurger component

---
+++ src/shared/UI/MenuBurger/assets/index.ts
@@ -0,0 +1,5 @@
+import profileIcon from './profile.svg';
+import cartIcon from './cart.svg';
+import cartBlackIcon from './cart-black.svg';
+
+export { profileIcon, cartIcon, cartBlackIcon };
---
+++ src/shared/UI/MenuBurger/index.ts
@@ -0,0 +1,1 @@
+export { MenuBurger } from './MenuBurger';
