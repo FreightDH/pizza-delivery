e279fc6e3502ac0ac54fd7b590d53b8bb78fc1c7
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-05-29 18:13:21 +0300

    feat: add CartPage component

--- src/pages/cart/CartPage.ui.tsx
+++ src/pages/cart/CartPage.ui.tsx
@@ -1,16 +1,56 @@
 import type { FC, ReactElement } from 'react';
 
 import { useAppSelector } from '@/shared/lib';
+import { CustomButton } from '@/shared/UI/CustomButton';
 
 import { EmptyCartPage } from './UI/EmptyCartPage';
+
+import { cartIcon, trashIcon } from './assets';
 import cl from './CartPage.module.scss';
+import { CartDish } from '@/widgets/CartDish';
 
 interface CartPageProps {}
 
 export const CartPage: FC<CartPageProps> = (): ReactElement => {
   const order = useAppSelector((state) => state.orderReducer.order);
+  const orderDishes = Object.entries(order.dishes);
 
   if (!order.dishesCount) return <EmptyCartPage />;
 
-  return <div>Корзина товаров</div>;
+  return (
+    <main className={cl.page}>
+      <div className={cl.page__container}>
+        <div className={cl.page__body}>
+          <div className={cl.page__header}>
+            <h1 className={cl.page__title}>
+              <img alt="cart-icon" src={cartIcon} /> Корзина
+            </h1>
+            <button className={cl.page__clear}>
+              <img alt="trash-icon" src={trashIcon} />
+              Очистить корзину
+            </button>
+          </div>
+          <div className={cl.page__order}>
+            <div className={cl.order__dishes}>
+              {orderDishes.map((dish, index) => (
+                <CartDish key={index} dish={dish} />
+              ))}
+            </div>
+            <div className={cl.order__info}>
+              <div className={cl.order__count}>
+                Всего пицц: <span> {order.dishesCount} шт.</span>
+              </div>
+              <div className={cl.order__sum}>
+                Сумма заказа: <span>{order.sum} ₽</span>
+              </div>
+            </div>
+          </div>
+          <div className={cl.page__footer}>
+            <CustomButton black>Вернуться назад</CustomButton>
+            <CustomButton primary>Оформить заказ</CustomButton>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
---
+++ src/pages/cart/assets/index.ts
@@ -0,0 +1,4 @@
+import cartIcon from './cart.svg';
+import trashIcon from './trash.svg';
+
+export { cartIcon, trashIcon };
